8b0abb248d53ca2b8c4f35bd91fe2d951717c273
Zachary Fedorak <fedorak04@hotmail.ca>
2022-03-07 14:20:07 -0700

    completed batch demo

--- dmit2015-batch-demo/src/main/java/dmit2015/dmit2015batchdemo/HelloApplication.java
+++ dmit2015-batch-demo/src/main/java/dmit2015/dmit2015batchdemo/HelloApplication.java
@@ -3,7 +3,7 @@ package dmit2015.dmit2015batchdemo;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/webapi")
 public class HelloApplication extends Application {
 
 }
